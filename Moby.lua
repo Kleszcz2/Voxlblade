2217ab9ed2c9037e71c18ca7e7559f7fbd519d34
Kleszcz2 <61806260+Kleszcz2@users.noreply.github.com>
2023-04-13 14:27:42 +0200

    Update Moby.lua

--- Moby.lua
+++ Moby.lua
@@ -1,148 +1,36 @@
 return function(Object: Instance)
-	local Light: PointLight = Object:FindFirstChildWhichIsA('PointLight')
-	local Mesh: SpecialMesh = Object:FindFirstChildWhichIsA("SpecialMesh")
-	local Particle: ParticleEmitter = Object:FindFirstChildWhichIsA('ParticleEmitter', true)
-	
-	if Object:IsA('MeshPart') then
-		if Object.MeshId == 'rbxassetid://5196551436' then
-			return {
-				Color = Color3.fromRGB(255, 213, 128),
-				Name = 'Amulet'
-			}
-		elseif Object.MeshId == 'rbxassetid://923469333' then
-			return {
-				Color = Color3.fromRGB(255, 213, 128),
-				Name = 'Candy'
-			}
-		elseif Object.MeshId == 'rbxassetid://5204003946' then
-			return {
-				Color = Color3.fromRGB(255, 213, 128),
-				Name = 'Goblet'
-			}
-		elseif Object.MeshId == 'rbxassetid://2520762076' then
-			return {
-				Color = Color3.fromRGB(96, 130, 182),
-				Name = 'Howler Friend'
-			}
-		elseif Object.MeshId == 'rbxassetid://5196577540' then
-			return {
-				Color = Color3.fromRGB(255, 213, 128),
-				Name = 'Old Amulet'
-			}
-		elseif Object.MeshId == 'rbxassetid://5196782997' then
-			return {
-				Color = Color3.fromRGB(255, 213, 128),
-				Name = 'Old Ring'
-			}
-		elseif Object.MeshId == 'rbxassetid://5196776695' then
-			return {
-				Color = Color3.fromRGB(255, 213, 128),
-				Name = 'Ring'
-			}
-		elseif Object.MeshId == 'rbxassetid://5204453430' then
-			return {
-				Color = Color3.fromRGB(96, 130, 182),
-				Name = 'Scroll'
-			}
-		end
-	elseif Object:IsA('Part') then
-		if Object.Color == Color3.fromRGB(89, 34, 89) then
-			return {
-				Color = Color3.fromRGB(96, 130, 182),
-				Name = '???'
-			}
-		elseif Object.Color == Color3.fromRGB(164, 187, 190) then
-			return {
-				Color = Color3.fromRGB(96, 130, 182),
-				Name = 'Diamond'
-			}
-		elseif Object.Color == Color3.fromRGB(0, 184, 49) then
-			return {
-				Color = Color3.fromRGB(96, 130, 182),
-				Name = 'Emerald'
-			}
-		elseif Object.Color == Color3.fromRGB(128, 187, 219) then
-			return {
-				Color = Color3.fromRGB(255, 0, 255),
-				Name = 'Fairfrozen'
-			}
-		elseif Object.Color == Color3.fromRGB(254, 85, 100) then
-			return {
-				Color = Color3.fromRGB(96, 130, 182),
-				Name = 'Ice Essence'
-			}
-		elseif Particle and Particle.Color.Keypoints[1].Value == Color3.new(0.45098, 1, 0) then
-			return {
-				Color = Color3.fromRGB(255, 213, 128),
-				Name = 'Mysterious Artifact'
-			}
-		elseif Object.Color == Color3.fromRGB(248, 248, 248) and Mesh then
-			return {
-				Color = Color3.fromRGB(255, 213, 128),
-				Name = 'Opal'
-			}
-		elseif Particle and Particle.Color.Keypoints[1].Value == Color3.new(1, 0.8, 0) then
-			return {
-				Color = Color3.fromRGB(96, 130, 182),
-				Name = 'Phoenix Down'
-			}
-		elseif Object.Color == Color3.fromRGB(255, 0, 191) then
-			return {
-				Color = Color3.fromRGB(255, 0, 255),
-				Name = 'Rift Gem'
-			}
-		elseif Object.Color == Color3.fromRGB(255, 0, 0) then
-			return {
-				Color = Color3.fromRGB(96, 130, 182),
-				Name = 'Ruby'
-			}
-		elseif Object.Color == Color3.fromRGB(16, 42, 220) then
-			return {
-				Color = Color3.fromRGB(96, 130, 182),
-				Name = 'Sapphire'
-			}
-		elseif Object.Color == Color3.fromRGB(255, 255, 0) then
-			return {
-				Color = Color3.fromRGB(255, 0, 255),
-				Name = 'Spider Cloak'
-			}
-		end
-	elseif Object:IsA('UnionOperation') then
-		if Light and Light.Color == Color3.fromRGB(255, 255, 255) then
-			return {
-				Color = Color3.fromRGB(255, 0, 255),
-				Name = 'Amulet of the White King'
-			}
-		elseif Object.Color == Color3.fromRGB(111, 113, 125) then
-			return {
-				Color = Color3.fromRGB(255, 213, 128),
-				Name = 'Idol of the Forgotten'
-			}
-		elseif Object.Color == Color3.fromRGB(248, 248, 248) and not Object.UsePartColor then
-			return {
-				Color = Color3.fromRGB(255, 0, 255),
-				Name = [[Lannis's Amulet]]
-			}
-		elseif Object.Color == Color3.fromRGB(29, 46, 58) then
-			return {
-				Color = Color3.fromRGB(255, 0, 255),
-				Name = 'Night Stone'
-			}
-		elseif Object.Color == Color3.fromRGB(255, 89, 89) then
-			return {
-				Color = Color3.fromRGB(255, 0, 255),
-				Name = [[Philosopher's Stone]]
-			}
-		elseif Object.Color == Color3.fromRGB(248, 217, 109) then
-			return {
-				Color = Color3.fromRGB(255, 0, 255),
-				Name = 'Scroom Key'
-			}
-		end
-	end
-	
-	return {
-		Color = Color3.fromRGB(255, 255, 255),
-		Name = 'Unknown'
-	}
+    local Ligt: PointLight = Object:FindFirstChildWhichIsA('PointLight')
+    local Mesh: SpecialMesh = Object:FindFirstChildWhichIsA("SpecialMesh")
+    local Particle: ParticleEmitter = Object:FindFirstChildWhichIsA('ParticleEmitter', true)
+    
+    if Object:IsA('MeshPart') then
+        if Object.Name == 'Buni' then
+            if Object.Bloody.Enabled == false and Object.Corrupt.Enabled == false and Object.Magical.Enabled == false and Object.Legendary.Enabled == false then
+                return {
+                    Color = Color3.fromRGB(255, 255, 255),
+                    Name = 'Buni'
+                }
+            elseif Object.Bloody.Enabled == true and Object.Corrupt.Enabled == false and Object.Magical.Enabled == false and Object.Legendary.Enabled == false then
+                return {
+                   Color = Color3.fromRGB(255, 0, 0),
+                   Name = 'Bloody Buni'
+                }
+            elseif Object.Bloody.Enabled == false and Object.Corrupt.Enabled == true and Object.Magical.Enabled == false and Object.Legendary.Enabled == false then
+                return {
+                    Color = Color3.fromRGB(128, 128, 128),
+                    Name = 'Corrupt Buni'
+                }
+            elseif Object.Bloody.Enabled == false and Object.Corrupt.Enabled == false and Object.Magical.Enabled == true and Object.Legendary.Enabled == false then
+                return {
+                    Color = Color3.fromRGB(102, 102, 255),
+                    Name = 'Magical Buni'
+                }
+            elseif Object.Bloody.Enabled == false and Object.Corrupt.Enabled == false and Object.Magical.Enabled == false and Object.Legendary.Enabled == true then
+                return {
+                    Color = Color3.fromRGB(255, 255, 0),
+                    Name = 'Legendary Buni'
+                }
+            end
+        end
+    end
 end
